e4744554325bb49ebfc4903f5a452fa75c5a0a53
raccoon86 <onehub86@gmail.com>
2020-05-26 22:01:46 +0900

    login 구현을 formView를 이용해서 쉽게 구현하기

--- users/views.py
+++ users/views.py
@@ -1,27 +1,35 @@
-from django.views import View
+from django.views.generic import FormView
 from django.shortcuts import render, redirect, reverse
+from django.urls import reverse_lazy
 from django.contrib.auth import authenticate, login, logout
 from . import forms
 
 
-class LoginView(View):
-    def get(self, request):
+class LoginView(FormView):
 
-        form = forms.LoginForm(initial={"user": "onehub86@likelion.org"})
+    template_name = "users/login.html"
+    form_class = forms.LoginForm
+    success_url = reverse_lazy("core:home")
 
-        return render(request, "users/login.html", context={"form": form})
+    def form_valid(self, form):
+        user = form.cleaned_data.get("user")
+        password = form.cleaned_data.get("password")
+        login_user = authenticate(self.request, username=user, password=password)
+        if login_user is not None:
+            login(self.request, login_user)
+        return super().form_valid(form)
 
-    def post(self, request):
+    # def post(self, request):
 
-        form = forms.LoginForm(request.POST)
-        if form.is_valid():
-            user = form.cleaned_data.get("user")
-            password = form.cleaned_data.get("password")
-            login_user = authenticate(request, username=user, password=password)
-            if login_user is not None:
-                login(request, login_user)
-                return redirect(reverse("core:home"))
-        return render(request, "users/login.html", context={"form": form})
+    #     form = forms.LoginForm(request.POST)
+    #     if form.is_valid():
+    #         user = form.cleaned_data.get("user")
+    #         password = form.cleaned_data.get("password")
+    #         login_user = authenticate(request, username=user, password=password)
+    #         if login_user is not None:
+    #             login(request, login_user)
+    #             return redirect(reverse("core:home"))
+    #     return render(request, "users/login.html", context={"form": form})
 
 
 def log_out(request):
