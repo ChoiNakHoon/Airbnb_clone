09bbb38f00c1665568484426cc430874f85c5994
raccoon86 <onehub86@gmail.com>
2020-06-04 18:29:09 +0900

    Profile Update #1
      - UpdateView를 이용해서 유저 프로필 수정 페이지 생성

--- rooms/urls.py
+++ rooms/urls.py
@@ -4,6 +4,6 @@ from rooms import views as rooms_views
 app_name = "rooms"
 
 urlpatterns = [
-    path("<int:pk>", rooms_views.DetailView.as_view(), name="detail"),
+    path("<int:pk>/", rooms_views.DetailView.as_view(), name="detail"),
     path("search/", rooms_views.SearchView.as_view(), name="search"),
 ]
