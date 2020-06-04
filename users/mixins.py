7cc60638b1e97483dc1f1c3c4ee755c10a268dbd
raccoon86 <onehub86@gmail.com>
2020-06-04 22:01:18 +0900

    mixins를 이용한 url 예외처리?
      -  유저의 상태에 따라 접근 할 수 없는 페이지를 url로 강제로 접근 할려고 했을때 의도 된 페이지로 이동 시키는 방법.

--- users/mixins.py
+++ users/mixins.py
@@ -1,15 +1,31 @@
-from django.shortcuts import redirect, reverse
-from django.contrib.auth.mixins import UserPassesTestMixin
+from django.contrib import messages
+from django.urls import reverse_lazy
+from django.shortcuts import redirect
+from django.contrib.auth.mixins import UserPassesTestMixin, LoginRequiredMixin
 
 
-# 유저가 로그인 했을 때 보여지면 안 되는 페이지를 url로 접근 할려고 했을 때
-# 메인 페이지로 이동
-class LoggedOutOnlyView(UserPassesTestMixin):
+# 유저가 로그인 했을 때 보여지면 안 되는 페이지를 url로 접근 할려고 했을 때 의도 된 페이지로 이동 시킴
 
-    permission_denied_message = "Page not found"
-
+# Email유저 이외 유저가 접근 할 수 없는 페이지를 url로 접근 하려고 했을 때
+class EmailLoginOnlyView(UserPassesTestMixin):
     def test_func(self):
-        return not self.request.user.is_authentucated
+        return self.request.user.login_method == "email"
 
     def handle_no_permission(self):
+        messages.error(self.request, "Can't go there")
         return redirect("core:home")
+
+
+# 로그인 유저가 접근 할 수 없는 페이지를 url로 접근 하려고 했을때.
+class LoggedOutOnlyView(UserPassesTestMixin):
+    def test_func(self):
+        return not self.request.user.is_authenticated
+
+    def handle_no_permission(self):
+        messages.error(self.request, "Can't go there")
+        return redirect("core:home")
+
+
+# 로그 아웃인 상태인 유저가 접근 할 수 없는 페이지를 url로 접근 하려고 했을 때
+class LoggedInOnlyView(LoginRequiredMixin):
+    login_url = reverse_lazy("users:login")
--- users/views.py
+++ users/views.py
@@ -15,7 +15,6 @@ class LoginView(mixins.LoggedOutOnlyView, FormView):
 
     template_name = "users/login.html"
     form_class = forms.LoginForm
-    success_url = reverse_lazy("core:home")
 
     def form_valid(self, form):
         email = form.cleaned_data.get("email")
@@ -24,6 +23,17 @@ class LoginView(mixins.LoggedOutOnlyView, FormView):
         if user is not None:
             login(self.request, user)
         return super().form_valid(form)
+
+    def get_success_url(self):
+        next_arg = self.request.GET.get("next")
+        if next_arg is not None:
+            messages.warning(self.request, f"Hi {self.request.user.first_name}")
+            return next_arg
+        else:
+            messages.success(
+                self.request, f"Welcome back {self.request.user.first_name}"
+            )
+            return reverse("core:home")
 
 
 def log_out(request):
@@ -227,7 +237,7 @@ class UserProfileView(DetailView):
     context_object_name = "user_obj"
 
 
-class UpdateProfileView(SuccessMessageMixin, UpdateView):
+class UpdateProfileView(mixins.LoggedInOnlyView, SuccessMessageMixin, UpdateView):
 
     model = models.User
     template_name = "users/update_profile.html"
@@ -258,7 +268,12 @@ class UpdateProfileView(SuccessMessageMixin, UpdateView):
     success_message = "Profile Updated"
 
 
-class UpdatePasswordView(SuccessMessageMixin, PasswordChangeView):
+class UpdatePasswordView(
+    mixins.LoggedInOnlyView,
+    mixins.EmailLoginOnlyView,
+    SuccessMessageMixin,
+    PasswordChangeView,
+):
 
     template_name = "users/update_password.html"
     success_message = "Password Changed"
